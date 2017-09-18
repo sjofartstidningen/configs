cf48baca8f36d331e2cb00f998caee6c4fc78965
Adam Bergman <adam@fransvilhelm.com>
2017-09-18 02:29:20 +0200

    :twisted_rightwards_arrows: Setup development environment (#2)
    * :wrench: Setup eslint and prettier
    
    * :pencil2: Format

--- packages/eslint-config-sjofartstidningen/index.js
+++ packages/eslint-config-sjofartstidningen/index.js
@@ -6,11 +6,7 @@ module.exports = {
     'eslint-config-prettier',
     'eslint-config-prettier/flowtype',
   ].map(require.resolve),
-  plugins: [
-    'import',
-    'flowtype',
-    'prettier',
-  ],
+  plugins: ['import', 'flowtype', 'prettier'],
   env: {
     commonjs: true,
     es6: true,
